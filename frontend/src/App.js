778ffddc3c9fbf04e679b79ba009becd96f65a2f
Italo-Yan <italoyan2004@gmail.com>
2024-09-24 11:15:56 -0300

    Removendo arquivos que não são necessários e começando a organizar o frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          O projeto está rodando!!!.
-        </p>
+        <h1>
+          Mindly
+        </h1>
       </header>
     </div>
   );
