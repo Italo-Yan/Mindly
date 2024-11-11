71ed8c4f843040a7e0a4d9e35709a05dcf3e129a
Amanda Kuehn <devamandakuehn@gmail.com>
2024-11-11 14:05:31 -0300

    Responsividade: about, box, description, form, header e services

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./Header.module.css";
 
 export function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleNavigation = (sectionId, event) => {
     if (event) event.preventDefault();
-
     navigate("/", { replace: true });
     setTimeout(() => {
       window.location.hash = sectionId;
     }, 50);
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
@@ -19,8 +24,12 @@ export function Header() {
         <NavLink to="/">
           <h1>Mindly</h1>
         </NavLink>
+        
+        <button className={styles.menuButton} onClick={toggleMenu}>
+          ☰
+        </button>
 
-        <ul>
+        <ul className={`${styles.navMenu} ${menuOpen ? styles.showMenu : ""}`}>
           <li>
             <a href="#home" onClick={(event) => handleNavigation("home", event)}>
               Início
